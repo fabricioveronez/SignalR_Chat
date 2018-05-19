ed4abeb38d31a43b2044933136c73ed1f769841c
DESKTOP-OL596BN\fabri <fabricioveronez@gmail.com>
2018-05-18 22:05:45 -0300

    Inclusão do hub de chat

---
+++ src/server/SignalR_Chat.Web/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR_Chat.Web.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Chat.Web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task EnviarMensagem(Mensagem mensagem)
+        {
+            await Clients.All.SendAsync("RecebendoMensagem", mensagem);
+        }
+    }
+  
+}
