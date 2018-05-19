3461ff3ea93d4b0e9e267ff20878c11920b6a845
DESKTOP-OL596BN\fabri <fabricioveronez@gmail.com>
2018-05-18 23:31:17 -0300

    Inclusão do bootstrap e o formulário pra enviar mensagem.

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className='container'>
+        <form>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" onChange={e => this.setValue('mensagem', e)} />
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="button">Button</button>
+            </div>
+          </div>     
+        </form>
+      </div> 
     );
   }
 }
--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
