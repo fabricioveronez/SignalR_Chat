c7e12402c1b94073bd335615be28cd389e57cae8
DESKTOP-OL596BN\fabri <fabricioveronez@gmail.com>
2018-05-19 23:11:15 -0300

    Contador de usuários online

--- src/server/SignalR_Chat.Web/Hubs/ChatHub.cs
+++ src/server/SignalR_Chat.Web/Hubs/ChatHub.cs
@@ -9,10 +9,27 @@ namespace SignalR_Chat.Web.Hubs
 {
     public class ChatHub : Hub
     {
+
+        private static int contador;
+
         public async Task EnviarMensagem(Mensagem mensagem)
         {
             await Clients.All.SendAsync("RecebendoMensagem", mensagem);
         }
+
+        public override async Task OnConnectedAsync()
+        {
+            contador++;
+            Console.WriteLine(string.Format("{0} usuários online.", contador));
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            contador--;
+            Console.WriteLine(string.Format("{0} usuários online.", contador));
+            await base.OnDisconnectedAsync(exception);
+        }
     }
-  
+
 }
